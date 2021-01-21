4fefe09dcf02f6e538f7b273d0cfff421cfd6006
tornado <mb.masoumi1000@gmail.com>
2021-01-21 16:39:39 +0330

    gg

--- app/src/main/java/com/example/widget2/ExampleAppwidgetProvider.java
+++ app/src/main/java/com/example/widget2/ExampleAppwidgetProvider.java
@@ -3,12 +3,14 @@ package com.example.widget2;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class ExampleAppwidgetProvider extends AppWidgetProvider {
+    private static int mCounter = 0;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -29,6 +31,7 @@ public class ExampleAppwidgetProvider extends AppWidgetProvider {
             PendingIntent pendingIntent2 = PendingIntent.getBroadcast(context, 0, intent2, 0);
             remoteView.setOnClickPendingIntent(R.id.btn2, pendingIntent2);
 
+            remoteView.setTextViewText(R.id.txt_count,"0");
 
             appWidgetManager.updateAppWidget(appWidgetId, remoteView);
         }
@@ -39,7 +42,18 @@ public class ExampleAppwidgetProvider extends AppWidgetProvider {
         super.onReceive(context, intent);
 
         if(intent.getAction().equals("btn2")){
-            Toast.makeText(context, "btn2 clicked !", Toast.LENGTH_SHORT).show();
+
+            mCounter++;
+            RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+
+            remoteView.setTextViewText(R.id.txt_count,String.valueOf(mCounter));
+//            Toast.makeText(context, "btn2 clicked !", Toast.LENGTH_SHORT).show();
+
+            // This time we dont have widgetId. Reaching our widget with that way.
+            ComponentName appWidget = new ComponentName(context, ExampleAppwidgetProvider.class);
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidget, remoteView);
         }
     }
 }
